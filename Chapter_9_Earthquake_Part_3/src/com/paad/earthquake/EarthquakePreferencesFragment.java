45fa0a9d21d515813979ae21beb28ae9db03b292
Wilbur Burk <mooseandpebs@gmail.com>
2016-05-30 15:39:40 -0700

    initial build

---
+++ Chapter_9_Earthquake_Part_3/src/com/paad/earthquake/EarthquakePreferencesFragment.java
@@ -0,0 +1,63 @@
+package com.paad.earthquake;
+
+import java.util.List;
+
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+
+public class EarthquakePreferencesFragment extends PreferenceFragment 
+implements OnSharedPreferenceChangeListener {
+	static final String TAG = "EarthquakePreferencesFragment";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.userpreferences);
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		view.setBackgroundColor(Color.parseColor("#ffd480"));
+		super.onViewCreated(view, savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		getPreferenceManager().getSharedPreferences()
+		.registerOnSharedPreferenceChangeListener(this);
+
+		boolean update =
+		getPreferenceManager().getSharedPreferences() 
+			.getBoolean(getResources().getString(R.string.auto_update_key), false);
+
+	}
+	
+}

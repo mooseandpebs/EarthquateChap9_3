e3a32a2872c727260bd011dd6e0fb5cd51e09fc6
Wilbur Burk <mooseandpebs@gmail.com>
2016-06-01 07:36:36 -0700

    added code to EarthquakeSearchResultsFragment

--- Chapter_9_Earthquake_Part_3/src/com/paad/earthquake/EarthquakeSearchResultsFragment.java
+++ Chapter_9_Earthquake_Part_3/src/com/paad/earthquake/EarthquakeSearchResultsFragment.java
@@ -1,17 +1,34 @@
 package com.paad.earthquake;
 
 import android.app.ListFragment;
+import android.app.SearchManager;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
+import android.content.CursorLoader;
+import android.content.Intent;
 
 public class EarthquakeSearchResultsFragment extends ListFragment implements LoaderCallbacks<Cursor> {
 
+	private SimpleCursorAdapter mAdapter;
+	
 	public EarthquakeSearchResultsFragment() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mAdapter = new SimpleCursorAdapter(getActivity(),android.R.layout.simple_expandable_list_item_1,null,
+				new String[] {EarthquakeProvider.KEY_SUMMARY},new int[] {android.R.id.text1},0);
+		setListAdapter(mAdapter);
+		getLoaderManager().initLoader(0, null, this);
+		parseIntent(getActivity().getIntent());
+	}
+	
+	
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 		// TODO Auto-generated method stub
@@ -29,5 +46,23 @@ public class EarthquakeSearchResultsFragment extends ListFragment implements Loa
 		// TODO Auto-generated method stub
 		
 	}
+
+  private static String QUERY_EXTRA_KEY = "QUERY_EXTRA_KEY";
+	
+  private void parseIntent(Intent intent) {
+	    // If the Activity was started to service a Search request,
+	    // extract the search query.
+	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+	      String searchQuery = intent.getStringExtra(SearchManager.QUERY);
+
+	      // Perform the search, passing in the search query as an argument
+	      // to the Cursor Loader
+	      Bundle args = new Bundle();
+	      args.putString(QUERY_EXTRA_KEY, searchQuery);
+	      
+	      // Restart the Cursor Loader to execute the new query.
+	      getLoaderManager().restartLoader(0, args, this);
+	    }
+	  }
 
 }
